ffe2da959349b8ad2dcd92eba768be27bf10085f
scorpich <kdanik@i.ua>
2024-05-23 14:41:42 +0300

    first commit

---
+++ learning_logs/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+     text = models.CharField(max_length=200)
+     date_added = models.DateTimeField(auto_now_add=True)
+
+     def __str__(self):
+         return self.text
+     
+class Rule(models.Model):
+    rule = models.CharField(max_length=255)
+    ban = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.rule
+    
+class Entry(models.Model):
+     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+     text = models.TextField()
+     date_added = models.DateTimeField(auto_now_add=True)
+    
+     class Meta:
+         verbose_name_plural = 'entries'
+    
+     def __str__(self):
+         return f"{self.text[:50]}..."
